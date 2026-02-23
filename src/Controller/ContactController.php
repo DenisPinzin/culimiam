c4be16d751b896fd91547c25ec2836eb6279eb46
DenisPinzin <d.pinzinsro@gmail.com>
2026-02-23 15:17:05 +0100

    [FIX] modify contactController for destinataire

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -23,7 +23,7 @@ final class ContactController extends AbstractController
 
             $email = (new Email())
                 ->from('contact@culimiam.sc5duyo5958.universe.wf')
-                ->to('contact@culimiam.sc5duyo5958.universe.wf')
+                ->to($data['email'])
                 ->subject('FORMULAIRE ' . date('H:i:s'))
                 ->text(
                     "Nom: {$data['name']}\n".
