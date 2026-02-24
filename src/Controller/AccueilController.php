6f575da43a5cc88e85a5e0b32b8a2f68aedb7f58
DenisPinzin <d.pinzinsro@gmail.com>
2026-02-24 14:32:55 +0100

    [NEW] add more content on accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RecetteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,9 +10,16 @@ use Symfony\Component\Routing\Attribute\Route;
 final class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(RecetteRepository $recetterepository): Response
     {
+        $lastcard = $recetterepository->findBy(
+            [],                 
+            ['id' => 'DESC'],   
+            6                   
+        );
+
         return $this->render('accueil/index.html.twig', [
+            'lastcard' => $lastcard,
             'controller_name' => 'AccueilController',
         ]);
     }
