d8d57a72477356976888d153bd8e06ada42ae86a
DenisPinzin <d.pinzinsro@gmail.com>
2026-05-25 23:12:00 +0200

    [NEW] add filters on recettes

--- src/Controller/RecettesController.php
+++ src/Controller/RecettesController.php
@@ -3,19 +3,40 @@
 namespace App\Controller;
 
 use App\Repository\RecetteRepository;
+use App\Repository\TyperepasRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class RecettesController extends AbstractController
 {
     #[Route('/recettes', name: 'app_recettes')]
-    public function index(RecetteRepository $recetteRepository): Response
+    public function index(Request $request, RecetteRepository $recetteRepository, TyperepasRepository $typerepasRepository): Response
     {   
-        $recettes = $recetteRepository->findAll();
+        // récupère ?mine=1 dans l'URL
+        $mine = $request->query->getBoolean('mine');
+
+        // récupère ?type=1, ?type=2, etc.
+        $typeId = $request->query->get('type');
+
+        // si "mes recettes" est demandé, on prend l'utilisateur connecté, sinon null = pas de filtre utilisateur
+        $user = $mine ? $this->getUser() : null;
+
+        // récupère les recettes filtrées
+        $recettes = $recetteRepository->findFiltered(
+            $user,
+            $typeId ? (int) $typeId : null
+        );
+
+        // récupère tous les types pour le select
+        $typesRepas = $typerepasRepository->findAll();
 
         return $this->render('recettes/index.html.twig', [
             'recettes' => $recettes,
+            'typesRepas' => $typesRepas,
+            'mine' => $mine,
+            'selectedType' => $typeId,
         ]);
     }
 }
--- src/Repository/RecetteRepository.php
+++ src/Repository/RecetteRepository.php
@@ -5,6 +5,8 @@ namespace App\Repository;
 use App\Entity\Recette;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\User;
+
 
 /**
  * @extends ServiceEntityRepository<Recette>
@@ -14,6 +16,52 @@ class RecetteRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Recette::class);
+    }
+
+
+    public function findFiltered(?User $user = null, ?int $typeId = null): array
+    {
+        // creation du builder sur l'entité recette "r" devient l'alias de Recette
+        $qb = $this->createQueryBuilder('r')
+
+        // jointure avec typerepas
+        ->leftJoin('r.typerepas', 't')
+        ->addSelect('t')
+
+        // jointure avec User
+        ->leftJoin('r.user', 'u')
+        ->addSelect('u');
+
+        //FILTRE RECETTE
+        // si un utilisateur est envoyé à la méthode alors on filtre uniquement ses recettes
+        if ($user) {
+
+            $qb->andWhere('r.user = :user')
+
+                // Remplace :user par l'utilisateur connecté
+                ->setParameter('user', $user);
+        }
+
+        //FILTRE TYPE REPAS
+        // si un type de repas est sélectionné
+        if ($typeId) {
+
+            $qb->andWhere('r.typerepas = :typeId')
+
+                // Remplace typeId par l'id du type sélectionné
+                ->setParameter('typeId', $typeId);
+        }
+
+        return $qb
+
+            // recettes de la plus récente à la plus ancienne
+            ->orderBy('r.id', 'DESC')
+
+            // execute la requête SQL
+            ->getQuery()
+
+            // retourne les résultats
+            ->getResult();
     }
 
     //    /**
